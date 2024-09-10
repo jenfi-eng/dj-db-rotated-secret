e2c2f0e322b8edb433afe044f53a5ed47e2bc9a7
Justin Louie <224840+nitsujri@users.noreply.github.com>
2024-09-10 11:20:47 +0800

    Better log outputs

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "Running tests with psycopg2"
+echo "=================Running tests with psycopg2================="
+echo ""
+
+echo "  - Removing .venv"
 rm -rf .venv
+
+echo "  - Recreating venv"
 poetry install --with dev,psycopg2 --without psycopg3
+
+echo "  - Running tests"
 poetry run pytest
 
-echo "Running tests with psycopg3"
+echo "=================Running tests with psycopg3================="
+echo ""
+
+echo "  - Removing .venv"
 rm -rf .venv
+
+echo "  - Recreating venv"
 poetry install --with dev,psycopg3 --without psycopg2
+
+echo "  - Running tests"
 poetry run pytest
