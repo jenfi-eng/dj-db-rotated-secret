3067ef1aefd0130d269a1b5cd26565ed0dd4f01a
Justin Louie <224840+nitsujri@users.noreply.github.com>
2024-09-10 10:30:55 +0800

    Can't find a better way to isolate psycopg versions

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Running tests with psycopg2"
+rm -rf .venv
 poetry install --with dev,psycopg2 --without psycopg3
 poetry run pytest
 
 echo "Running tests with psycopg3"
+rm -rf .venv
 poetry install --with dev,psycopg3 --without psycopg2
 poetry run pytest
